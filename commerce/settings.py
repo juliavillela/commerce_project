0b7853bfb484a882e5507845be0bc538bb5653db
juliavillela <33360385+juliavillela@users.noreply.github.com>
2020-10-19 18:50:41 -0300

    updates allowed hosts

--- commerce/settings.py
+++ commerce/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '6ps8j!crjgrxt34cqbqn7x&b3y%(fny8k8nh21+qa)%ws3fh!q'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['commerce-project-auctions.heroku.com']
+ALLOWED_HOSTS = ['commerce-project-auctions.herokuapp.com']
 
 CSRF_COOKIE_SECURE = True
 
