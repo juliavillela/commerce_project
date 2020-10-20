752d351a5f9447385b7bf0c654d26e9347c33414
juliavillela <33360385+juliavillela@users.noreply.github.com>
2020-10-20 12:17:37 -0300

    updates database default

--- commerce/settings.py
+++ commerce/settings.py
@@ -84,9 +84,15 @@ LOGIN_URL = '/login'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'CONN_MAX_AGE': 500,
+    }
 }
+db_from_env = dj_database_url.config()
 
+DATABASES['default'].update(db_from_env)
 
 AUTH_USER_MODEL = 'auctions.User'
 
