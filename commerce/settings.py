927718b039f6ac36b8e838fbc9cf9665ea8d82fe
juliavillela <33360385+juliavillela@users.noreply.github.com>
2020-10-20 14:00:47 -0300

    testing withoud db specs

--- commerce/settings.py
+++ commerce/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import dj_database_url
+# import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -87,12 +87,12 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'CONN_MAX_AGE': 500,
+        # 'CONN_MAX_AGE': 500,
     }
 }
-db_from_env = dj_database_url.config()
+# db_from_env = dj_database_url.config()
 
-DATABASES['default'].update(db_from_env)
+# DATABASES['default'].update(db_from_env)
 
 AUTH_USER_MODEL = 'auctions.User'
 
